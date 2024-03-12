ee9aa0d812a03878b4bfb9134f77723c993e21d2
Shreya29p <sp3223326@gmail.com>
2024-03-12 16:06:29 +0530

    my first commit

---
+++ corejava/ArrayDemo.java
@@ -0,0 +1,14 @@
+public class ArrayDemo
+{
+    public static void main(String[] args)
+    {
+        int[] marks =new int[5];
+        marks[0]=69;
+        marks[1]=58;
+        marks[2]=89;
+        marks[3]=70;
+        marks[4]=56;
+
+        System.out.println(marks[0]);
+    }
+}
